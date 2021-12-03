d506a577726083703c97ee326ce7683d0c70e6b5
mmirk <t-mmirk@hotels.com>
2021-12-03 18:08:22 +0100

    fixed the order of expected/actual values

--- src/test/kotlin/PriceSpec.kt
+++ src/test/kotlin/PriceSpec.kt
@@ -3,33 +3,33 @@ import io.kotest.matchers.shouldBe
 
 class PriceSpec : StringSpec({
     "test totals" {
-        0 shouldBe price("")
-        50 shouldBe price("A")
-        80 shouldBe price("AB")
-        115 shouldBe price("CDBA")
-        100 shouldBe price("AA")
-        130 shouldBe price("AAA")
-        180 shouldBe price("AAAA")
-        230 shouldBe price("AAAAA")
-        260 shouldBe price("AAAAAA")
-        160 shouldBe price("AAAB")
-        175 shouldBe price("AAABB")
-        190 shouldBe price("AAABBD")
-        190 shouldBe price("DABABA")
+        price("") shouldBe 0
+        price("A") shouldBe 50
+        price("AB") shouldBe 80
+        price("CDBA") shouldBe 115
+        price("AA") shouldBe 100
+        price("AAA") shouldBe 130
+        price("AAAA") shouldBe 180
+        price("AAAAA") shouldBe 230
+        price("AAAAAA") shouldBe 260
+        price("AAAB") shouldBe 160
+        price("AAABB") shouldBe 175
+        price("AAABBD") shouldBe 190
+        price("DABABA") shouldBe 190
     }
 
     "test incremental" {
         val co = CheckOut(RULES)
         0 shouldBe co.total()
-        co.scan('A'); 50 shouldBe co.total()
-        co.scan('B'); 80 shouldBe co.total()
-        co.scan('A'); 130 shouldBe co.total()
-        co.scan('A'); 160 shouldBe co.total()
-        co.scan('B'); 175 shouldBe co.total()
+        co.scan('A'); co.total() shouldBe 50
+        co.scan('B'); co.total() shouldBe 80
+        co.scan('A'); co.total() shouldBe 130
+        co.scan('A'); co.total() shouldBe 160
+        co.scan('B'); co.total() shouldBe 175
     }
 })
 
-val RULES: Any = TODO()
+val RULES: Set<PricingRule> = TODO()
 
 fun price(goods: String): Int {
     val co = CheckOut(RULES)

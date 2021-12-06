80dda9bf6e3c19d8bb394222c3350e3defb8501c
mmirk <t-mmirk@hotels.com>
2021-12-06 01:47:13 +0100

    implementation with interface and decorator

--- src/main/kotlin/CheckOut.kt
+++ src/main/kotlin/CheckOut.kt
@@ -1,12 +1,32 @@
-/**
- * TODO: Implement me...
- */
-class CheckOut(rules: Any) { // define your own "set of rules" type
+class CheckOut(val rules: Set<PricingRule>) {
+    val scanned = mutableListOf<Char>()
+
     fun scan(item: Char) {
-        TODO("Not yet implemented")
+        scanned.add(item)
     }
 
-    fun total(): Int {
-        TODO("Not yet implemented")
+    fun total() = rules.sumOf { it.calculate(scanned) }
+}
+
+interface PricingRule {
+    fun calculate(items: List<Char>): Int
+    fun appliesTo(item: Char): Boolean
+}
+
+class FixedPrice(val item: Char, val unitPrice: Int) : PricingRule {
+    override fun calculate(items: List<Char>) = items.count(this::appliesTo) * unitPrice
+    override fun appliesTo(item: Char) = item == this.item
+}
+
+class SpecialPrice(val base: PricingRule, val units: Int, val discountedPrice: Int) : PricingRule by base {
+    override fun calculate(items: List<Char>): Int {
+        val applicable = items.filter(this::appliesTo)
+        return discountedPartial(applicable) + basePartial(applicable)
     }
+
+    private fun discountedPartial(applicable: List<Char>) =
+        applicable.size / units * discountedPrice
+
+    private fun basePartial(applicable: List<Char>) =
+        base.calculate(applicable.take(applicable.size % units))
 }
--- src/test/kotlin/PriceSpec.kt
+++ src/test/kotlin/PriceSpec.kt
@@ -1,35 +1,45 @@
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class PriceSpec : StringSpec({
     "test totals" {
-        price("") shouldBe 0
-        price("A") shouldBe 50
-        price("AB") shouldBe 80
-        price("CDBA") shouldBe 115
-        price("AA") shouldBe 100
-        price("AAA") shouldBe 130
-        price("AAAA") shouldBe 180
-        price("AAAAA") shouldBe 230
-        price("AAAAAA") shouldBe 260
-        price("AAAB") shouldBe 160
-        price("AAABB") shouldBe 175
-        price("AAABBD") shouldBe 190
-        price("DABABA") shouldBe 190
+        assertSoftly {
+            price("") shouldBe 0
+            price("A") shouldBe 50
+            price("AB") shouldBe 80
+            price("CDBA") shouldBe 115
+            price("AA") shouldBe 100
+            price("AAA") shouldBe 130
+            price("AAAA") shouldBe 180
+            price("AAAAA") shouldBe 230
+            price("AAAAAA") shouldBe 260
+            price("AAAB") shouldBe 160
+            price("AAABB") shouldBe 175
+            price("AAABBD") shouldBe 190
+            price("DABABA") shouldBe 190
+        }
     }
 
     "test incremental" {
         val co = CheckOut(RULES)
-        0 shouldBe co.total()
-        co.scan('A'); co.total() shouldBe 50
-        co.scan('B'); co.total() shouldBe 80
-        co.scan('A'); co.total() shouldBe 130
-        co.scan('A'); co.total() shouldBe 160
-        co.scan('B'); co.total() shouldBe 175
+        assertSoftly {
+            0 shouldBe co.total()
+            co.scan('A'); co.total() shouldBe 50
+            co.scan('B'); co.total() shouldBe 80
+            co.scan('A'); co.total() shouldBe 130
+            co.scan('A'); co.total() shouldBe 160
+            co.scan('B'); co.total() shouldBe 175
+        }
     }
 })
 
-val RULES: Set<PricingRule> = TODO()
+val RULES = setOf(
+    SpecialPrice(FixedPrice('A', 50), units = 3, discountedPrice = 130),
+    SpecialPrice(FixedPrice('B', 30), units = 2, discountedPrice = 45),
+    FixedPrice('C', 20),
+    FixedPrice('D', 15),
+)
 
 fun price(goods: String): Int {
     val co = CheckOut(RULES)
